f0198661ecd4432cdc56d0210c902827623d3436
Melon-Lord-07 <nitchis@educ.college-montreal.qc.ca>
2023-01-17 09:10:59 -0500

    Initial commit

---
+++ blank_window.py
@@ -0,0 +1,12 @@
+import arcade
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+
+def main():
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Tutoriel Arcade")
+    arcade.run()
+
+
+main()
---
+++ draw_circle.py
@@ -0,0 +1,27 @@
+import arcade
+
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height, title):
+        # Call the parent class's init function
+        super().__init__(width, height, title)
+
+
+def main():
+    window = MyGame(680, 420, "Drawing Example")
+    arcade.run()
+
+
+def on_draw():
+    """
+    C'est la méthode que Arcade invoque à chaque "frame" pour afficher les éléments
+    de votre jeu à l'écran.
+    """
+
+    arcade.start_render()
+
+    arcade.draw_circle_filled(10, 10, 20, (255, 54, 34))
+
+
+main()
+on_draw()
