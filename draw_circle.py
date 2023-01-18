4ac01fa79b7cd52e0087cb01fe04e909d6c3237c
Melon-Lord-07 <nitchis@educ.college-montreal.qc.ca>
2023-01-18 10:21:08 -0500

    commit librairie arcade fini le cercle

--- draw_circle.py
+++ draw_circle.py
@@ -6,22 +6,23 @@ class MyGame(arcade.Window):
         # Call the parent class's init function
         super().__init__(width, height, title)
 
+    def on_draw(self):
+        """
+        C'est la méthode que Arcade invoque à chaque "frame" pour afficher les éléments
+        de votre jeu à l'écran.
+        """
+
+        arcade.set_background_color(arcade.color.BABY_BLUE)
+        arcade.start_render()
+
+        arcade.draw_circle_filled(300, 300, 50, arcade.color.CASTLETON_GREEN)
+
+        arcade.finish_render()
+
 
 def main():
     window = MyGame(680, 420, "Drawing Example")
-    arcade.run()
-
-
-def on_draw():
-    """
-    C'est la méthode que Arcade invoque à chaque "frame" pour afficher les éléments
-    de votre jeu à l'écran.
-    """
-
-    arcade.start_render()
-
-    arcade.draw_circle_filled(10, 10, 20, (255, 54, 34))
+    window.run()
 
 
 main()
-on_draw()
